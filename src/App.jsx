9b2ff90318d4f12205eeb336921dacd532731658
Ayub <mdayubansari2014@gmail.com>
2024-09-12 14:07:12 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,16 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import CheckoutPage from "./Pages/CheckOutPage";
+// import CheckoutPage from "./Pages/CheckOutPage";
 import ConfirmationPage from "./Pages/ConfirmationPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/checkout" element={<CheckoutPage />} />
+        {/* <Route path="/checkout" element={<CheckoutPage />} /> */}
         <Route path="/confirm" element={<ConfirmationPage />} />
-        <Route path="*" element={<Navigate to="/checkout" />} />
+        <Route path="*" element={<Navigate to="/confirm" />} />
       </Routes>
     </Router>
   );
